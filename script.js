2e7136060a77888b48670dd16a13544f25716669
ramu siripalli <ramsiripalli2772@gmail.com>
2026-02-03 13:58:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+
+let level = 0;
+
+// traverse up the DOM tree
+while (element) {
+  level++;
+  element = element.parentElement;
+}
+
+alert("The level of the element is: " + level);
