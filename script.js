0d5cbda3f452f8868106b71b8f0dcd81034d08aa
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-30 16:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+
+let current = element;
+
+let count = 0;
+
+while(current){
+
+    count++;
+
+    current = current.parentElement;
+
+}
+
+alert(`The level of the element is: ${count}`);
